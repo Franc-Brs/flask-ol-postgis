65866a68d7441ccfd46c845168c41307725613a7
Franc-Brs <francesco.bursi@hotmail.it>
2022-06-10 21:04:51 +0200

    upload files enabled

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -1,14 +1,18 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, redirect, url_for, flash
 from .models import db, City
 import geoalchemy2.functions as func
 import json
-
+from werkzeug.utils import secure_filename
 from sqlalchemy import select
+import os
 
 app = Flask(__name__)
 app.config.from_object("project.config.Config")
+app.secret_key = "secret key"
 #db = SQLAlchemy(app)
 db.init_app(app)
+
+from .functions import allowed_file
 
 @app.route("/<int:celsius>")
 def fahrenheit_from(celsius):
@@ -18,7 +22,7 @@ def fahrenheit_from(celsius):
     fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
     return str(fahrenheit)
 
-@app.route('/')
+@app.route('/maps')
 def root_ol():
    markers=[
        {
@@ -70,3 +74,34 @@ def test():
     }
     
     return jsonify(layer)
+
+
+@app.route('/upload', methods=['POST','GET'])
+def upload_file():
+    
+    if request.method == 'POST':
+        # check if the post request has the file part
+        #files = request.files.getlist("file[]")
+        #for f in files:
+        #    print(f)
+        
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+          
+        file = request.files['file']
+        if file.filename == '':
+            flash('No file selected for uploading')
+            return redirect(request.url)
+        
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash('File successfully uploaded')
+            return redirect('/upload')
+        else:
+            flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
+            return redirect(request.url)
+    
+    return render_template('upload.html')
+
--- services/web/project/config.py
+++ services/web/project/config.py
@@ -4,6 +4,11 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
+
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite://")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOAD_FOLDER = os.path.join(basedir, 'uploads')
+    if not os.path.isdir(UPLOAD_FOLDER):
+        os.mkdir(UPLOAD_FOLDER)
+    ALLOWED_EXTENSIONS = set(['shp', 'shx', 'prj', 'dbf'])
